c436ab8b87b6b53ce71f839b01c820094a0275bc
avartation <36676864+avartation@users.noreply.github.com>
2018-04-14 15:11:58 +0400

    Create Singleton.swift

---
+++ Singleton.swift
@@ -0,0 +1,57 @@
+class City {
+    public let name: String
+    public let area: Int
+    public let population: Int
+    public let foundationYear: String
+    public let timeZone: String
+    public init(name: String, area: Int, population: Int, founded: String, timeZone: String) {
+        self.name = name
+        self.area = area
+        self.population = population
+        self.foundationYear = founded
+        self.timeZone = timeZone 
+    }
+    public func describe() {
+        print(name + " City")
+        print("area        : \(area) km^2")
+        print("population  : \(population)")
+        print("founded in  : \(foundationYear)")
+        print("time zone   : \(timeZone)", terminator: "\n\n")
+    }
+}
+
+class Yerevan: City {
+    private init() {
+        super.init(name: "Yerevan", area: 223, population: 1060138, founded: "782 BC", timeZone: "UTC+4")
+    }
+
+    private static let uniqueInstance = Yerevan()
+    public static func getInstance() -> Yerevan {
+        return uniqueInstance
+    }
+    public static func describe() {
+        uniqueInstance.describe()
+    }
+}
+
+class NewYork: City {
+    private init() {
+        super.init(name: "New York", area: 789, population: 8538000, founded: "1624 AC", timeZone: "UTC-5")
+    }
+
+    private static let uniqueInstance = NewYork()
+    public static func getInstance() -> NewYork {
+        return uniqueInstance
+    }
+    public static func describe() {
+        uniqueInstance.describe()
+    }
+}
+
+
+
+Yerevan.describe()
+NewYork.describe()
+
+
+
